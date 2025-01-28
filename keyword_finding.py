27c7ffc9d9daedc27cb6d431d6ae6ba1c803b013
lillemor <lillemor.haibach@swisstopo.ch>
2025-01-28 13:40:05 +0100

    finnd toc by keyword, pattern

--- keyword_finding.py
+++ keyword_finding.py
@@ -1,8 +1,5 @@
 from utils import TextWord, TextLine
 import regex
-from typing import Union
-import fitz
-
 
 
 def find_keyword(word: TextWord, keywords: list[str]) -> TextWord:
@@ -28,32 +25,3 @@ def find_keywords_in_lines(text_lines: list[TextLine],keywords : list[str]):
 
 def get_keywords_by_language(language, params):
     return params.get("table_of_contents", {}).get(language, [])
-    
-
-class TOC:
-    def __init__(self, entries: list[dict[str, Union[str, int]]]):
-        """
-        Initializes a TOC instance.
-
-        :param entries: A list of TOC entries. Each entry is a dictionary with keys:
-            - "heading" (str): The title of the TOC entry.
-            - "page" (int): The page number where the heading points.
-            - Optional: Additional metadata (e.g., level, section hierarchy).
-        """
-        self.entries = entries
-
-    def __repr__(self):
-        return f"TOC(entries={self.entries})"
-
-    def to_dict(self) -> dict:
-        """Converts the TOC to a dictionary format for JSON serialization."""
-        return {"entries": self.entries}
-
-
-def extract_by_bookmarks(doc: fitz.Document) :
-        """Extracts TOC using PDF bookmarks."""
-        toc = doc.get_toc()
-        if toc:
-            entries = [{"heading": title, "page": page, "level": level} for level, title, page in toc]
-            return TOC(entries=entries)
-        return None
