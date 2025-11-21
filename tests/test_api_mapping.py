9aa06d74926477922584d34163cdb8e9d0eeb8a2
Stijn Vermeeren <stijn.vermeeren@swisstopo.ch>
2025-11-21 10:22:30 +0100

    fix unit test by using PascalPageClasses

--- tests/test_api_mapping.py
+++ tests/test_api_mapping.py
@@ -1,19 +1,18 @@
 import pytest
 
-from api.app.v1.schemas import predicted_class
-from src.page_classes import PageClasses
+from api.app.v1.schemas import PascalPageClasses, predicted_class
 
 
 @pytest.mark.parametrize(
     "classes,expected",
     [
-        ({"text": 0, "boreprofile": 1, "map": 0, "title_page": 0, "unknown": 0}, PageClasses.BOREPROFILE),
-        ({"text": 0, "boreprofile": 0, "map": 1, "title_page": 0, "unknown": 0}, PageClasses.MAP),
-        ({"text": 0, "boreprofile": 0, "map": 0, "title_page": 0, "unknown": 0}, PageClasses.UNKNOWN),
-        ({"TEXTTT": 1, "boreprofile": 0, "map": 0, "title_page": 0, "unknown": 0}, PageClasses.UNKNOWN),
+        ({"text": 0, "boreprofile": 1, "map": 0, "title_page": 0, "unknown": 0}, PascalPageClasses.BOREPROFILE),
+        ({"text": 0, "boreprofile": 0, "map": 1, "title_page": 0, "unknown": 0}, PascalPageClasses.MAP),
+        ({"text": 0, "boreprofile": 0, "map": 0, "title_page": 0, "unknown": 0}, PascalPageClasses.UNKNOWN),
+        ({"TEXTTT": 1, "boreprofile": 0, "map": 0, "title_page": 0, "unknown": 0}, PascalPageClasses.UNKNOWN),
     ],
 )
-def test_mapping_to_stable_labels(classes: dict[str, int], expected: PageClasses):
+def test_mapping_to_stable_labels(classes: dict[str, int], expected: PascalPageClasses):
     """Test label stable mapping.
 
     Args:
@@ -21,4 +20,4 @@ def test_mapping_to_stable_labels(classes: dict[str, int], expected: PageClasses
         expected (PageClasses): Expected page class enum.
     """
     page_pred = predicted_class(classes)
-    assert page_pred.name == expected.name
+    assert page_pred == expected
