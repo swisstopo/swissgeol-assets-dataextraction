e9ff6732e81b57ae6598176438f8024d2a6d8824
agatha_schmidt <agatha.schmidt@visium.com>
2026-03-20 13:11:43 +0100

    change model path temp

--- src/constants.py
+++ src/constants.py
@@ -1,4 +1,5 @@
 """Project-wide constants."""
 
 # Default model path for the stable TreeBased classifier
-DEFAULT_TREEBASED_MODEL_PATH = "models/stable/model.joblib"
+# DEFAULT_TREEBASED_MODEL_PATH = "models/stable/model.joblib"
+DEFAULT_TREEBASED_MODEL_PATH = "models/xgboost_model_24v2/20260320-115958/model.joblib"
